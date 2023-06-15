2d442bf960cbcb7ce739759f5d557f0195232c7e
TSUJI Nanoka <s1300015@sshsv211.u-aizu.ac.jp>
2023-06-15 09:48:25 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 import java.util.Random;
 class DiceGame{
     public static void main(String[] args){
         int[] dice = {1, 2, 3, 4, 5, 6};
         int total = 0;
+        String name;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
 
         Random r = new Random();
         System.out.println("Rolling dice...");
@@ -12,5 +19,12 @@ class DiceGame{
             System.out.println("Die " + i + ": " + num);
         }
         System.out.println("Total value: "  + total);
+
+        if(total > 7){
+            System.out.println(name + " won");
+        }
+        else{
+            System.out.println(name + " lost");
+        }
     }
 }
